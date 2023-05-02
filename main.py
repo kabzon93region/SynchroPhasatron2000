828d4611b29c19542783f32631e6422cb5bd3825
kabzon93region <kabzon93region@gmail.com>
2023-05-02 15:54:45 +0300

    1
    добавил окно настроек для редактирования списка на синхронизацию, собственно список на синхронизацию реализован тоже и работает даже если папки не существует он её создаст.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+# coding: utf8
 from datetime import datetime as dt
 from tkinter import filedialog
 from pathlib import Path
@@ -7,69 +9,20 @@ import time
 import os
 
 
-def WCFG():
-    windowCFG = window = tk.Toplevel() #tk.Tk()
-    windowCFG.protocol("WM_DELETE_WINDOW", lambda: closeWCFG(windowCFG))
-    windowCFG.title("Новое окно")
-    xw = 865  # задаем ширину окна
-    yw = 420  # задаем высоту окна
-    xspos = (windowCFG.winfo_screenwidth() - xw) / 2  # рассчитываем отступ по ширине для создания окна на экране по центру
-    yspos = (windowCFG.winfo_screenheight() - yw) / 2  # рассчитываем отступ по высоте для создания окна на экране по центру
-    windowCFG.geometry("%dx%d+%d+%d" % (xw, yw, xspos, yspos))  # задаем ширину, высоту окна и отступы по ширине и высоте для создания окна
-    #windowCFG.resizable(False, False)  # задаем возможность изменять размер окна по ширине, высоте
-    windowCFG.config(bg=bgcolor)  # задаем цвет фона в hex формате
-    windowCFG.grab_set() 
-
-    for i in range(14):
-        windowCFG.grid_rowconfigure(i, minsize=30)
-        if i <= 8:
-            windowCFG.grid_columnconfigure(i, minsize=100)
-
-    entc1 = tk.Entry(windowCFG)
-    entc1.grid(row=0, column=0, columnspan=5, rowspan=1, sticky='nwes')
-    entc2 = tk.Entry(windowCFG)
-    entc2.grid(row=1, column=0, columnspan=5, rowspan=1, sticky='nwes')
+dirList = []
+try:
+    cfgFile = open("config.cfg", 'r', encoding='Windows-1251')
+    for line in cfgFile:
+        strLine = line.replace("\n", '')
+        dirList.append(strLine)
+    cfgFile.close()
+except FileNotFoundError:
+    cfgFile = open("config.cfg", 'w', encoding='Windows-1251')
+    cfgFile.close()
 
 
-    wcfgBtn1=tk.Button(windowCFG, text="OK", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: closeWCFG(windowCFG))
-    wcfgBtn1.grid(row=0, column=7, columnspan=1, rowspan=1, sticky='nwes')
-    wcfgBtn2=tk.Button(windowCFG, text="Отмена", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: closeWCFG(windowCFG))
-    wcfgBtn2.grid(row=1, column=7, columnspan=1, rowspan=1, sticky='nwes')
-
-    wcfgBtn3=tk.Button(windowCFG, text="Выбрать исходную папку", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn1_f(entc1))
-    wcfgBtn3.grid(row=0, column=5, columnspan=1, rowspan=1, sticky='nwes')
-    wcfgBtn4=tk.Button(windowCFG, text="Выбрать конечную папку", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn2_f(entc2))
-    wcfgBtn4.grid(row=1, column=5, columnspan=1, rowspan=1, sticky='nwes')
-
-    wcfgBtn3=tk.Button(windowCFG, text="Сохранить", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn1_f(entc1))
-    wcfgBtn3.grid(row=0, column=6, columnspan=1, rowspan=1, sticky='nwes')
-    wcfgBtn4=tk.Button(windowCFG, text="Удалить", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
-                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn2_f(entc2))
-    wcfgBtn4.grid(row=1, column=6, columnspan=1, rowspan=1, sticky='nwes')
-
-    dir_lb = tk.Listbox(windowCFG, bg=bgcolor, fg=txtcolor, selectbackground=actbgcolor, selectforeground=acttxtcolod, font=fontvar)
-    dir_lb.grid(row=2, column=0, sticky='nwes', columnspan=8, rowspan=13)
-
-    dir_lb.insert('end', "C:\Python\pyProjects\\test_bkp_copy\\f1===>>>C:\Python\pyProjects\\test_bkp_copy\\f2")
-    dir_lb.insert('end', "C:\Python\pyProjects\\test_bkp_copy\\f3===>>>C:\Python\pyProjects\\test_bkp_copy\\f4")
 
 
-def closeWCFG(W: tk.Tk):
-    try:
-        btn4.config(state="normal")
-    except:
-        print("error: no found window as tk.Tk()")
-    finally:                
-        W.grab_release()
-        W.destroy() 
-
-
-# задаем пути к папкам 1 и 2
 window = tk.Tk()
 bgcolor = '#1E1F22'
 actbgcolor = '#575A63'
@@ -105,7 +58,7 @@ btn1.grid(row=0, column=0, sticky='nwes', columnspan=3)
 btn2 = tk.Button(window, text="Выбрать конечную папку", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor, activeforeground=acttxtcolod, font=fontvar, command=lambda: btn2_f(ent2, textarea))
 btn2.grid(row=1, column=0, sticky='nwes', columnspan=3)
 btn3 = tk.Button(window, text="Старт", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor, activeforeground=acttxtcolod,
-                font=fontvar, command=lambda: cff_timer(str(ent1.get()), str(ent2.get()), textarea))
+                font=fontvar, command=lambda: startSync(textarea))
 btn3.grid(row=0, column=4, sticky='nwes', columnspan=1, rowspan=2)
 #print(btn3.cget('text'))
 btn4 = tk.Button(window, text="Настройки", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor, activeforeground=acttxtcolod, font=fontvar, command=lambda: btn4_f())
@@ -138,6 +91,45 @@ def btn2_f(ent: tk.Entry, console_area: tk.Text = None):
         console_area.see('end')        
         window.update()
     print(f"{now}: Конечная папка выбрана: '{ent.get()}'")
+
+
+def startSync(console_area: tk.Text):
+    start_timer = time.time()  # точка отсчета времени работы программы
+
+    for list in dirList:
+        msgList = list.split(" ===>>> ")            
+        dir1 = str(msgList[0])
+        dir2 = str(msgList[1])
+        cff_timer(str(dir1), str(dir2), console_area)
+
+    end_timer = time.time() - start_timer  # собственно время работы программы
+    now = dt.now().strftime("%H:%M:%S:%f")
+    console_area.insert('end', f"\n{now}: Синхронизация списка завершена за {end_timer} секунд\n")
+    console_area.see('end')
+    window.update()
+    print(f"{now}: Синхронизация списка завершена за {end_timer} секунд")
+    
+
+def cff_timer(source_folder, dest_folder, console_area: tk.Text):
+    if(btn3.cget('text')=="Старт"):
+        #btn3.config(state="disabled")
+        start_time = time.time()  # точка отсчета времени работы программы
+
+        btn3.config(text="Стоп")        
+
+        copy_files(source_folder, dest_folder, console_area)
+
+        btn3.config(text="Старт")
+
+        end_time = time.time() - start_time  # собственно время работы программы
+        now = dt.now().strftime("%H:%M:%S:%f")
+        console_area.insert('end', f"\n{now}: Синхронизация завершена за {end_time} секунд\n")
+        console_area.see('end')
+        window.update()
+        print(f"{now}: Синхронизация завершена за {end_time} секунд")
+        #btn3.config(state="normal")
+    else:
+        btn3.config(text="Старт")
 
 
 def copy_files(source_folder=None, dest_folder=None, console_area: tk.Text = None):  # рекурсивная функция для копирования файлов
@@ -250,25 +242,138 @@ def copy_files(source_folder=None, dest_folder=None, console_area: tk.Text = Non
 # def copy_files
 
 
-def cff_timer(source_folder, dest_folder, console_area: tk.Text):
-    if(btn3.cget('text')=="Старт"):
-        #btn3.config(state="disabled")
-        start_time = time.time()  # точка отсчета времени работы программы
+def WCFG():
+    windowCFG = window = tk.Toplevel() #tk.Tk()
+    windowCFG.protocol("WM_DELETE_WINDOW", lambda: cencelWCFG(windowCFG))
+    windowCFG.title("Новое окно")
+    xw = 865  # задаем ширину окна
+    yw = 420  # задаем высоту окна
+    xspos = (windowCFG.winfo_screenwidth() - xw) / 2  # рассчитываем отступ по ширине для создания окна на экране по центру
+    yspos = (windowCFG.winfo_screenheight() - yw) / 2  # рассчитываем отступ по высоте для создания окна на экране по центру
+    windowCFG.geometry("%dx%d+%d+%d" % (xw, yw, xspos, yspos))  # задаем ширину, высоту окна и отступы по ширине и высоте для создания окна
+    #windowCFG.resizable(False, False)  # задаем возможность изменять размер окна по ширине, высоте
+    windowCFG.config(bg=bgcolor)  # задаем цвет фона в hex формате
+    windowCFG.grab_set() 
 
-        btn3.config(text="Стоп")        
+    for i in range(14):
+        windowCFG.grid_rowconfigure(i, minsize=30)
+        if i <= 8:
+            windowCFG.grid_columnconfigure(i, minsize=100)
 
-        copy_files(source_folder, dest_folder, console_area)
+    entc1 = tk.Entry(windowCFG)
+    entc1.grid(row=0, column=0, columnspan=6, rowspan=1, sticky='nwes')
+    entc2 = tk.Entry(windowCFG)
+    entc2.grid(row=1, column=0, columnspan=6, rowspan=1, sticky='nwes')
 
-        btn3.config(text="Старт")
 
-        end_time = time.time() - start_time  # собственно время работы программы
-        now = dt.now().strftime("%H:%M:%S:%f")
-        console_area.insert('end', f"\n{now}: Синхронизация завершена за {end_time} секунд\n")
-        console_area.see('end')
-        window.update()
-        print(f"{now}: Синхронизация завершена за {end_time} секунд")
-        #btn3.config(state="normal")
-    else:
-        btn3.config(text="Старт")
+    wcfgBtn1=tk.Button(windowCFG, text="OK", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: okWCFG(windowCFG, dir_lb))
+    wcfgBtn1.grid(row=0, column=7, columnspan=1, rowspan=1, sticky='nwes')
+    wcfgBtn2=tk.Button(windowCFG, text="Отмена", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: cencelWCFG(windowCFG))
+    wcfgBtn2.grid(row=1, column=7, columnspan=1, rowspan=1, sticky='nwes')
+
+    wcfgBtn3=tk.Button(windowCFG, text="Выбрать исходную папку", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn1_f(entc1))
+    wcfgBtn3.grid(row=0, column=6, columnspan=1, rowspan=1, sticky='nwes')
+    wcfgBtn4=tk.Button(windowCFG, text="Выбрать конечную папку", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: btn2_f(entc2))
+    wcfgBtn4.grid(row=1, column=6, columnspan=1, rowspan=1, sticky='nwes')
+
+    wcfgBtn3=tk.Button(windowCFG, text="Редактировать", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: selItem(dir_lb))
+    wcfgBtn3.grid(row=2, column=0, columnspan=2, rowspan=1, sticky='nwes')
+    wcfgBtn3=tk.Button(windowCFG, text="Сохранить", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: saveItem(dir_lb))
+    wcfgBtn3.grid(row=2, column=2, columnspan=2, rowspan=1, sticky='nwes')
+    wcfgBtn4=tk.Button(windowCFG, text="Удалить", bg=bgcolor, fg=txtcolor, activebackground=actbgcolor,
+                        activeforeground=acttxtcolod, font=fontvar, command=lambda: deleteItem(dir_lb))
+    wcfgBtn4.grid(row=2, column=4, columnspan=2, rowspan=1, sticky='nwes')
+
+    dir_lb = tk.Listbox(windowCFG, bg=bgcolor, fg=txtcolor, selectbackground=actbgcolor, selectforeground=acttxtcolod, font=fontvar)
+    dir_lb.grid(row=3, column=0, sticky='nwes', columnspan=8, rowspan=12)
+
+    for line in dirList:
+        dir_lb.insert('end', line)
+
+
+    def selItem(ListBox: tk.Listbox = None):
+        selected_indices= ListBox.curselection()
+        if len(selected_indices) > 0:
+            selected_langs = ",".join([ListBox.get(i) for i in selected_indices])
+            msg = selected_langs
+            msg = str(msg)
+            msgList = msg.split(" ===>>> ")            
+            entc1.delete(0, 'end')
+            entc1.insert(0, str(msgList[0]))
+            entc2.delete(0, 'end')
+            entc2.insert(0, str(msgList[1]))
+        else:
+            tk.messagebox.showinfo("Информационное сообщение", "Не выбран в списке элемент для редактирования.") #, command=open_info)
+    
+    def saveItem(ListBox: tk.Listbox = None):
+        if len(entc1.get()) > 0 and len(entc2.get()) > 0 :
+            selected_indices: str = str(ListBox.curselection())
+            if len(selected_indices) > 2:
+                disallowed_characters = "(,) "
+                for character in disallowed_characters:
+                    selected_indices = selected_indices.replace(character, "")
+                selected_indices = int(selected_indices)
+                msgList = f"{entc1.get()} ===>>> {entc2.get()}"
+                dir_lb.delete(selected_indices, selected_indices)
+                dir_lb.insert(selected_indices, msgList)
+            else:
+                msgList = f"{entc1.get()} ===>>> {entc2.get()}"
+                dir_lb.insert('end', msgList)
+        else:
+            tk.messagebox.showinfo("Информационное сообщение", "Не заполнены поля путей папок для сохранения в список.") #, command=open_info)
+
+    def deleteItem(ListBox: tk.Listbox = None):
+        selected_indices: str = str(ListBox.curselection())
+        if len(selected_indices) > 2:
+            disallowed_characters = "(,) "
+            for character in disallowed_characters:
+                selected_indices = selected_indices.replace(character, "")
+            selected_indices = int(selected_indices)
+            dir_lb.delete(selected_indices, selected_indices)
+        else:
+            tk.messagebox.showinfo("Информационное сообщение", "Не выбран элемент списка для удаления.") #, command=open_info)
+
+
+    def okWCFG(W: tk.Tk, dirlb: tk.Listbox):
+        dirList.clear()
+        for x in dirlb.get(0, 'end'):
+            dirList.append(x)
+
+        cfgFile = open("config.cfg", 'w', encoding='Windows-1251')
+        for line in dirList:
+            cfgFile.write(f"{line}\n")
+        cfgFile.close()
+
+        try:
+            btn4.config(state="normal")
+        except:
+            print("error: no found window as tk.Tk()")
+        finally:
+            W.grab_release()
+            W.destroy() 
+
+
+def cencelWCFG(W: tk.Tk):
+    try:
+        btn4.config(state="normal")
+    except:
+        print("error: no found window as tk.Tk()")
+    finally:                
+        W.grab_release()
+        W.destroy() 
+
+
+
+
+
+
+
+
 
 window.mainloop()
